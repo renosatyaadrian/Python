1c36ad648a9702085b99ee7fb019652dda32be6c
renosatyaadrian <renosatyaadrian@gmail.com>
2020-04-26 22:37:18 +0700

    Edited

---
+++ code/loop2.py
@@ -0,0 +1,34 @@
+nama_band = ['Payung Teduh',
+            'Sehela on Seben',
+            'Gigi Hadid',
+            'Mr. Tokoel'
+            ]
+
+lagu = ['Cicak Didinding',
+        'Lelayu di Musim Dingin',
+        'Halo Halo di Meja',
+        'Enak to Mantep to'
+        ]
+
+#enumerate
+for index.band in enumerate(nama_band):
+    print(index,':',band)
+
+#zip
+for band.lagu in zip(nama_band,lagu)
+    print(band,'menyanyikan lagu:',lagu)
+
+#set
+alphabet = {'b','s','d','a','w','p','a'}
+
+for a in sorted(alphabet)
+    print(a)
+
+print(" "*100)
+#dictionary
+jodoh = {"Aku":"Kamu",
+        "Dia":"Bukan Kamu",
+        "Kakekku":"Nenekmu"}
+
+for i in jodoh.items()
+    print(i)
